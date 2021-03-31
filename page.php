ddb03b5fabbc63b87632600c37ce800a3705863c
bimonestle <bimonestle@yahoo.co.id>
2021-03-31 23:42:46 +0700

    inject vue in about page

---
+++ page.php
@@ -0,0 +1,16 @@
+<?php
+
+get_header();
+?>
+<div class="wrap">
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+			<div id="wp-vue-app">
+				This will be replaced by
+			</div>
+		</main><!-- #main -->
+	</div><!-- #primary -->
+</div><!-- .wrap -->
+
+<?php
+get_footer();
